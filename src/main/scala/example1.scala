30406ed875fed26e78f5db89e2f9f393cfaf487a
Gabriele Lanaro <gabriele.lanaro@gmail.com>
2015-11-04 14:02:24 -0800

    List reverse

--- src/main/scala/example1.scala
+++ src/main/scala/example1.scala
@@ -1,5 +1,19 @@
 object Example1 {
-  def exec(args: Array[String]) {
-    println("Hi!")
-  }
+    // How to reverse a list
+    def exec(args: Array[String]) {
+    println("Example on how to reverse a list in scala")
+    val input: List[Int] = List(0, 1, 2, 3, 4)
+    val expected: List[Int] = List(4, 3, 2, 1, 0)
+    
+    println(reverse(input))
+
+    }
+    def reverse[A] (list: List[A]) : List[A] = {
+        list match {
+            case h :: tail =>
+                reverse(tail) ::: List(h)
+            case _ =>
+                Nil
+        }
+    }
 }
