93024e77caad5f9571b49793fdf89eee126f7d92
Gabriele Lanaro <gabriele.lanaro@gmail.com>
2015-11-05 14:00:39 -0800

    Back

--- src/main/scala/Example2.scala
+++ src/main/scala/Example2.scala
@@ -4,24 +4,43 @@ object Example2 {
         val input: Layer = new Layer(4)
         val hidden: Layer = new Layer(2)
         val output: Layer = new Layer(2)
-        
+
         val nn: Network = new Network(input, hidden, output)
-        
-        Array(0, 0, 0, 0)
-        Array(0, 0)
-        
+
+        nn.predict(Array(0, 0, 0, 0))
+
         println(input)
-    } 
+    }
 }
 
-class Network(layers: Layer*)
+class Network(layers: Layer*) {
+  def predict(input: Array[Double]): Array[Double] = {
+    var intermediate = input
+    for (layer <- layers) {
+      val activations = layer.activate(intermediate)
+      // weights = layer.weights
+      // intermediate = (activations zip weights)
+    }
+    Array(0, 0)
+  }
+}
 
 class Layer(size: Int) {
-    val list: List[Neuron] = for (i <- List.range(0, size)) yield new Neuron(0.0)
+    val neurons: List[Neuron] = for (i <- List.range(0, size)) yield new Neuron(0.0)
+    val weights = neurons.map(_.weigth)
+
+    def activate(input: Array[Double]): Array[Double] = {
+      (neurons, input).zipped map(_ activate _) toArray
+    }
 }
 
 class Neuron (weight: Double) {
-    override def toString() = {
-        s"Neuron(${weight})"
-    }
+  val weight = weight
+  override def toString() = {
+      s"Neuron(${weight})"
+  }
+
+  def activate(input: Double): Double = {
+    input
+  }
 }
